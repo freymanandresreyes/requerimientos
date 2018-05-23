3060ffcde7ba600f91182e9b99f67fb3f38f8073
freymanreyes <anserito74@gmail.com>
2018-05-23 10:57:16 -0500

    migraciones listas

---
+++ database/migrations/2018_05_23_085024_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->integer('id_estado')->unsigned();
+            $table->foreign('id_estado')->references('id')->on('estados');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
